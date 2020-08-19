989c7c8cc128ed201281e6bc5a1112688c02b65a
huanghusheng <2313303458@qq.com>
2020-08-19 23:18:23 +0800

    //

--- app/src/main/java/com/supertouch/MainActivity.java
+++ app/src/main/java/com/supertouch/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ARouter.getInstance().build("/login/loginPage").navigation();
-        finish();
+        //finish();
         // Hello
     }
 }
